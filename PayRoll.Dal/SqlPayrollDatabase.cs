bc8c48fe4d98d183aeca38e1e79f505781bbd9a5
chinaq <aaa6263@sina.com>
2014-06-11 14:16:49 +0800

    be able to insert into tb_DirectDepsitAccount and tb_PayCheckAddress

--- PayRoll.Dal/SqlPayrollDatabase.cs
+++ PayRoll.Dal/SqlPayrollDatabase.cs
@@ -8,10 +8,14 @@ using System.Data.SqlClient;
 
 namespace PayRoll.DAL
 {
-    public class SqlPayrollDatabase: PayrollDatabase
+    public class SqlPayrollDatabase : PayrollDatabase
     {
 
         private readonly SqlConnection connection;
+        private string methodCode;
+        private SqlCommand insertPaymentMethodCommand;
+
+
 
         public SqlPayrollDatabase()
         {
@@ -26,6 +30,9 @@ namespace PayRoll.DAL
 
         public void AddEmployee(Employee employee)
         {
+            PrepareToSavePaymentMethod(employee);
+
+
             string sql = "insert into Employee values(@EmpId, @Name, @Address, " +
                 "@ScheduleType, @PaymentMethodType, @PaymentClassificationType)";
             SqlCommand command = new SqlCommand(sql, connection);
@@ -34,11 +41,20 @@ namespace PayRoll.DAL
             command.Parameters.AddWithValue("@Name", employee.Name);
             command.Parameters.AddWithValue("@Address", employee.Address);
             command.Parameters.AddWithValue("@ScheduleType", ScheduleCode(employee.Schedule));
-            command.Parameters.AddWithValue("@PaymentMethodType", employee.Method.GetType().ToString());
+
+            //SavePaymentMethod(employee);
+            command.Parameters.AddWithValue("@PaymentMethodType", methodCode);
             command.Parameters.AddWithValue("@PaymentClassificationType", employee.Classification.GetType().ToString());
 
             command.ExecuteNonQuery();
+
+            if (insertPaymentMethodCommand != null)
+                insertPaymentMethodCommand.ExecuteNonQuery();
         }
+
+
+
+
 
         public Employee GetEmployee(int id)
         {
@@ -76,8 +92,45 @@ namespace PayRoll.DAL
         {
             if (schedule is MonthlySchedule)
                 return "monthly";
+            else if (schedule is WeeklySchedule)
+                return "weekly";
+            else if (schedule is BiWeeklySchedule)
+                return "biweekly";
             else
                 return "unknown";
         }
+
+
+
+        private void PrepareToSavePaymentMethod(Employee employee)
+        {
+            PaymentMethod method = employee.Method;
+            if (method is HoldMethod)
+                methodCode = "hold";
+            else if (method is DirectMethod)
+            {
+                methodCode = "directdeposit";
+                DirectMethod directMethod = method as DirectMethod;
+                string sql = "insert into DirectDepositAccount " +
+                    "values (@Bank, @Account, @EmpId)";
+                insertPaymentMethodCommand = new SqlCommand(sql, connection);
+                insertPaymentMethodCommand.Parameters.AddWithValue("@Bank", directMethod.Bank);
+                insertPaymentMethodCommand.Parameters.AddWithValue("@Account", directMethod.Account);
+                insertPaymentMethodCommand.Parameters.AddWithValue("@EmpId", employee.EmpId);
+            }
+            else if (method is MailMethod)
+            {
+                methodCode = "mail";
+                MailMethod mailMethod = method as MailMethod;
+                string sql = "insert into PayCheckAddress " +
+                    "values (@Address, @EmpId)";
+                insertPaymentMethodCommand = new SqlCommand(sql, connection);
+                insertPaymentMethodCommand.Parameters.AddWithValue("@Address", mailMethod.Address);
+                insertPaymentMethodCommand.Parameters.AddWithValue("@EmpId", employee.EmpId);
+            }
+            else
+                methodCode = "unknown";
+        }
+
     }
 }
--- PayRoll.UnitTest/DAL/SqlPayrollDatabaseTest.cs
+++ PayRoll.UnitTest/DAL/SqlPayrollDatabaseTest.cs
@@ -16,6 +16,7 @@ namespace PayRoll.UnitTest.DAL
     {
         private SqlPayrollDatabase database;
         private SqlConnection connection;
+        private Employee employee;
 
 
         [SetUp]
@@ -27,7 +28,25 @@ namespace PayRoll.UnitTest.DAL
                 @"Initial Catalog = PayRoll; Data Source = (local)\SQLSERVER2008; " +
                 "user id = sa; password = sa1203as0321");
             connection.Open();
-            SqlCommand command = new SqlCommand("delete from Employee", connection);
+
+
+            ClearDbTables();
+            employee = new Employee(123, "George", "123 Baker St.");
+            employee.Schedule = new MonthlySchedule();
+            employee.Method = new DirectMethod("Bank 1", "12389");
+            employee.Classification = new SalariedClassifiction(1000.00);
+
+        }
+
+        private void ClearDbTables()
+        {
+            SqlCommand command = new SqlCommand("delete from DirectDepositAccount", connection);
+            command.ExecuteNonQuery();
+
+            command = new SqlCommand("delete from PayCheckAddress", connection);
+            command.ExecuteNonQuery();
+
+            command = new SqlCommand("delete from Employee", connection);
             command.ExecuteNonQuery();
         }
 
@@ -41,19 +60,11 @@ namespace PayRoll.UnitTest.DAL
 
 
         [Test]
-        public void AddEmployee()
+        public void AddEmployeeTest_CheckBasicInfo_InEmployeeTable()
         {
-            Employee employee = new Employee(123, "George", "123 Baker St.");
-            employee.Schedule = new MonthlySchedule();
-            employee.Method = new DirectMethod("Bank 1", "12389");
-            employee.Classification = new SalariedClassifiction(1000.00);
+
             database.AddEmployee(employee);
-                        
-            SqlCommand command = new SqlCommand("select * from Employee", connection);
-            SqlDataAdapter adapter = new SqlDataAdapter(command);
-            DataSet dataset = new DataSet();
-            adapter.Fill(dataset);
-            DataTable table = dataset.Tables["table"];
+            DataTable table = LoadTable("Employee");
 
             Assert.AreEqual(1, table.Rows.Count);
             DataRow row = table.Rows[0];
@@ -64,25 +75,113 @@ namespace PayRoll.UnitTest.DAL
         }
 
 
+
         [Test]
-        public void ScheduleCodeTest()
+        public void AddEmployeeTest_CheckPaymentSchedule_InEmployeeTable()
         {
-            Employee employee = new Employee(123, "George", "123 Baker St.");
-            employee.Schedule = new MonthlySchedule();
-            employee.Method = new DirectMethod("Bank 1", "12389");
-            employee.Classification = new SalariedClassifiction(1000.00);
+            CheckSavedScheduleCode(new MonthlySchedule(), "monthly");
+            ClearDbTables();
+            CheckSavedScheduleCode(new WeeklySchedule(), "weekly");
+            ClearDbTables();
+            CheckSavedScheduleCode(new BiWeeklySchedule(), "biweekly");
+            ClearDbTables();
+        }
+
+
+
+
+        [Test]
+        public void AddEmployeeTest_CheckPaymentMethod_InEmployeeTable()
+        {
+            CheckSavedPaymentMethodCode(new HoldMethod(), "hold");
+            ClearDbTables();
+            CheckSavedPaymentMethodCode(new DirectMethod("Bank -1", "0987654321"), "directdeposit");
+            ClearDbTables();
+            CheckSavedPaymentMethodCode(new MailMethod("111 Maple Ct."), "mail");
+
+        }
+
+
+
+        [Test]
+        public void AddEmployeeTest_CheckDirectMethod_InDirectDepositAccountTable()
+        {
+            CheckSavedPaymentMethodCode(new DirectMethod("Bank -1", "0987654321"), "directdeposit");
+                        
+            DataTable table = LoadTable("DirectDepositAccount");
+
+            Assert.AreEqual(1, table.Rows.Count);
+            DataRow row = table.Rows[0];
+            Assert.AreEqual("Bank -1", row["Bank"]);
+            Assert.AreEqual("0987654321", row["Account"]);
+            Assert.AreEqual(123, row["EmpId"]);
+        }
+
+
+
+
+        [Test]
+        public void AddEmployeeTest_CheckMailMethod_InPayCheckAddressTable()
+        {
+            CheckSavedPaymentMethodCode(new MailMethod("111 Maple Ct."), "mail");
+
+            DataTable table = LoadTable("PaycheckAddress");
+
+            Assert.AreEqual(1, table.Rows.Count);
+            DataRow row = table.Rows[0];
+            Assert.AreEqual("111 Maple Ct.", row["Address"]);
+            Assert.AreEqual(123, row["EmpId"]);
+        }
+
+
+
+
+
+
+
+
+        private void CheckSavedPaymentMethodCode(PaymentMethod method, string expected)
+        {
+            employee.Method = method;
             database.AddEmployee(employee);
 
+            DataTable table = LoadTable("Employee");
+            Assert.AreEqual(1, table.Rows.Count);
+
+            DataRow row = table.Rows[0];
+            Assert.AreEqual(expected, row["PaymentMethiodType"]);
+        }
+
+
+
+
+
+
+
+
+        private void CheckSavedScheduleCode(PaymentSchedule schedule, string expected)
+        {
+            employee.Schedule = schedule;
+            database.AddEmployee(employee);
+
+            DataTable table = LoadTable("Employee");
+            Assert.AreEqual(1, table.Rows.Count);
+
+            DataRow row = table.Rows[0];
+            Assert.AreEqual(expected, row["PaymentScheduleType"]);
+        }
+
+
+
+        private DataTable LoadTable(String tableName)
+        {
             SqlCommand command = new SqlCommand(
-                "select * from Employee", connection);
+                "select * from "+tableName, connection);
             SqlDataAdapter adapter = new SqlDataAdapter(command);
             DataSet dataset = new DataSet();
             adapter.Fill(dataset);
             DataTable table = dataset.Tables["table"];
-
-            Assert.AreEqual(1, table.Rows.Count);
-            DataRow row = table.Rows[0];
-            Assert.AreEqual("monthly", row["ScheduleType"]);
+            return table;
         }
 
     }
